e6129112ef00c01565ce31039b1df03402ef2083
leeverson <306228032@qq.com>
2017-08-28 11:12:44 +0800

    Initial Commit

---
+++ GXXP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GXXP
+//
+//  Created by 大鸟网络 on 2017/8/28.
+//  Copyright © 2017年 com.bigbird. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GXXP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GXXP
+//
+//  Created by 大鸟网络 on 2017/8/28.
+//  Copyright © 2017年 com.bigbird. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
